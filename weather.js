4ba4990d5ea5137fd33e894cc87898626ea266fd
abr-ya <gamer05@list.ru>
2021-12-25 18:31:50 +0300

    feat: Add args helper

--- weather.js
+++ weather.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
+import { getArgs } from './helpers/args.js';
 
 const initCLI = () => {
-    console.log(process.argv);
+    const args = getArgs(process.argv);
+    console.log(args);
+
+    if (args.h) {
+        // вывод help
+    }
+
+    if (args.s) {
+        // сохранить город
+    }
+    
+    if (args.t) {
+        // сохранить токен
+    }
+
+    // вывести погоду
 };
 
 initCLI();
