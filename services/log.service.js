8d00f409654dd5997fded3a29d30f8a76a142bfc
abr-ya <gamer05@list.ru>
2022-01-01 23:58:55 +0300

    feat: Update printWeather function and prepare to publish

--- services/log.service.js
+++ services/log.service.js
@@ -20,16 +20,50 @@ const printHelp = () => {
 	);
 };
 
-const printWeather = (res, icon) => {
+// иконка погоды на основе кода
+const getIcon = (icon) => {
+	switch (icon.slice(0, -1)) {
+		case '01':
+			return '☀️';
+		case '02':
+			return '🌤️';
+		case '03':
+			return '☁️';
+		case '04':
+			return '☁️';
+		case '09':
+			return '🌧️';
+		case '10':
+			return '🌦️';
+		case '11':
+			return '🌩️';
+		case '13':
+			return '❄️';
+		case '50':
+			return '🌫️';
+	}
+};
+
+const getTimeZone = (apiZone) => `UTC${apiZone >= 0 ? "+" : ""}${apiZone / 3600}`
+
+const stamp2date = (stamp, timeZone='UTC') => {
+	const dateObject = new Date(stamp * 1000); // важно: миллисекнды!
+	return dateObject.toLocaleString('ru-RU', { timeZone });
+};
+
+const printWeather = (res) => {
 	if (!res) {
 		printError('Не передан объект результатов!');
 	} else {
+		const timeZone = getTimeZone(res.timezone);
 		console.log(
-			dedent`${chalk.bgYellow(' WEATHER ')} Погода в городе ${res.name}
-			${icon}  ${res.weather[0].description}
+			dedent`${chalk.bgBlue(' WEATHER ')} Погода в городе ${res.name} (${res.sys.country})
 			Температура: ${res.main.temp} (ощущается как ${res.main.feels_like})
+			Облачность: ${getIcon(res.weather[0].icon)}  ${res.weather[0].description}
 			Влажность: ${res.main.humidity}%
 			Скорость ветра: ${res.wind.speed}
+			Часовой пояс: ${timeZone}
+			Восход: ${stamp2date(res.sys.sunrise + res.timezone)} Закат: ${stamp2date(res.sys.sunset + res.timezone)}
 			`
 		);
 	}
--- weather.js
+++ weather.js
@@ -18,11 +18,29 @@ const saveToken = async (token) => {
     }
 };
 
+const saveCity = async (city) => {
+    if (!city.length || city.length < 3) {
+        printError('Введите имя города не меньше 3х символов!');
+        return
+    }
+    try {
+        // здесь можно бы сделать проверку на существование
+        // но надо или запросить погоду - тогда точно нужен токен
+        // или использовать альтернативный API с именами городов
+        await saveKeyValue('city', city);
+        printSuccess(`Город ${city} сохранён в вашем рабочем каталоге)`);
+    } catch (error) {
+        printError('Ошибка сохранения города!');
+        printError(error.message);
+    }
+};
+
 // основной запрос погоды сейчас здесь!
 // обработка ошибок будет здесь же
 const getForecast = async () => {
 	try {
-		const city = await getKeyValue(TOKEN_DICTIONARY.city);
+        // смотрим, нет ли в ENV?))
+		const city = process.env.CITY || await getKeyValue(TOKEN_DICTIONARY.city);
 		const weather = await getWeather(city || 'moscow');
 		printWeather(weather);
 	} catch (e) {
@@ -45,7 +63,7 @@ const initCLI = () => {
 
     if (args.s) {
         // сохранить город
-        saveKeyValue('city', args.s);
+        saveCity(args.s);
     }
     
     if (args.t) {
